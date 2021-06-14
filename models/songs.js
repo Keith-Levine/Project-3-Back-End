5a792f9c282a4ff39f9339a3a5d0d0495d6c4a28
Mitchell Debrah <debrahmitch@Mitchells-MacBook-Pro.local>
2021-06-14 15:09:41 -0400

    update

--- models/songs.js
+++ models/songs.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const songSchema = mongoose.Schema({
-  artists: {type: String, required: true},
-  songs: {type: String, required: true},
+  artist: {type: String, required: true},
+  song: {type: String, required: true},
 })
 
 module.exports = mongoose.model('Song', songSchema)
