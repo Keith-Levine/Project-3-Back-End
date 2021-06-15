b65a332834b26203ae96574c223ee7e849dbc1e0
Keith Levine <Levine.Keith@gmail.com>
2021-06-15 10:19:56 -0700

    server working locally, still needs remote

--- server.js
+++ server.js
@@ -2,22 +2,25 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+require('dotenv').config();
+
 const app = express()
-const PORT = 3003
+const PORT = process.env.PORT || 3003
+const MONGODBURI = process.env.MONGODBURI || 'mongodb://localhost:27017/stuffILike';
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/stuffILike', {
+mongoose.connect(MONGODBURI, {
      useNewUrlParser: true,
      useUnifiedTopology: true,
      useFindAndModify: false
 })
 
-mongoose.connection.once('open', ()=>{
+mongoose.connection.once('open', ()=> {
     console.log('connected to mongo :)')
 })
 
-const whitelist = ['http://localhost:3000']
+const whitelist = ['http://localhost:3000', 'https://sleepy-tor-42367.herokuapp.com/']
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
