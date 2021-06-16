45ab71780ce3f1129c0969ed18098b10ec73ebbf
Keith Levine <Levine.Keith@gmail.com>
2021-06-16 06:53:57 -0700

    cors finally working

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ mongoose.connection.once('open', ()=> {
     console.log('connected to mongo :)')
 })
 
-
-
 const whitelist = ['http://localhost:3000', 'https://sleepy-tor-42367.herokuapp.com']
 const corsOptions = {
   origin: function (origin, callback) {
